66e1022b70f6bb78fc96ab09f4ba85f3a323cf43
tourdehanoi <lacanrafael@orange.fr>
2025-03-22 10:11:08 +0100

    new piano

--- sources/piano.py
+++ sources/piano.py
@@ -1,133 +1,67 @@
-import os
-from classes import *
 import tkinter
-import threading
+from classes import *
 
-def get_kb_input():
-
-    if os.name == "nt":  #windows
-        import msvcrt
-        while True:
-            if msvcrt.kbhit(): #touche appuyée
-                key = msvcrt.getwch() #obtenir la touche
-                return key
-
-    else:  #GOOGLE (STACK OVERFLOW)
-
-        import termios
-        import tty
-        import sys
-        filedescriptors = termios.tcgetattr(sys.stdin)
-        tty.setcbreak(sys.stdin)
-        key = sys.stdin.read(1)[0]
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN,filedescriptors)
-        return key
-    
-# Dico de l'octave 3, celui du milieu
-
+# Key mappings for different octaves
 keymap3 = {
-    "s" : "do",
-    "d" : "re", 
-    "f" : "mi",
-    "g" : "fa", 
-    "h" : "sol",
-    "j" : "la",
-    "k" : "si",
-    "S" : "do#",
-    "D" : "re#",
-    "G" : "fa#",
-    "H" : "sol#",
-    "J" : "la#"
-}  
-
-#Dico de l'octave 2
+    "s": "do", "d": "re", "f": "mi", "g": "fa", "h": "sol", "j": "la", "k": "si",
+    "S": "do#", "D": "re#", "G": "fa#", "H": "sol#", "J": "la#"
+}
 
 keymap2 = {
-    "z" : "do",
-    "e" : "re", 
-    "r" : "mi",
-    "t" : "fa", 
-    "y" : "sol",
-    "u" : "la",
-    "i" : "si",
-    "Z" : "do#",
-    "E" : "re#",
-    "T" : "fa#",
-    "Y" : "sol#",
-    "U" : "la#"
-}  
-
-# Dico de l'octave 4
+    "z": "do", "e": "re", "r": "mi", "t": "fa", "y": "sol", "u": "la", "i": "si",
+    "Z": "do#", "E": "re#", "T": "fa#", "Y": "sol#", "U": "la#"
+}
 
 keymap4 = {
-    "x" : "do",
-    "c" : "re", 
-    "v" : "mi",
-    "b" : "fa", 
-    "n" : "sol",
-    "," : "la",
-    ";" : "si",
-    "X" : "do#",
-    "C" : "re#",
-    "B" : "fa#",
-    "N" : "sol#",
-    "?" : "la#"
-}  
+    "x": "do", "c": "re", "v": "mi", "b": "fa", "n": "sol", ",": "la", ";": "si",
+    "X": "do#", "C": "re#", "B": "fa#", "N": "sol#", "?": "la#"
+}
 
-def piano():
-    while True:
-        key = get_kb_input()  # Capture the keyboard input.
-        if key in keymap3:
-            note = Note(keymap3[key],3)
-            note.play()
-            print(note)
+# Function to handle key press and play the note
+def key_press(event):
+    key = event.char  # Get the key pressed
 
-        elif key in keymap2:
-            note = Note(keymap2[key],2)
-            note.play()
-            print(note)
+    # Check if the key is in any of the keymaps and play the corresponding note
+    if key in keymap3:
+        note = Note(keymap3[key], 3)
+        note.play()
+        print(note)
+    elif key in keymap2:
+        note = Note(keymap2[key], 2)
+        note.play()
+        print(note)
+    elif key in keymap4:
+        note = Note(keymap4[key], 4)
+        note.play()
+        print(note)
+    else:
+        print("Key pressed does nothing")
 
-        elif key in keymap4:
-            note = Note(keymap4[key],4)
-            note.play()
-            print(note)
-
-        else :
-            print("key pressed do nothing")
-
-
-#Initialiser fenetre
-window = tkinter.Tk() #créer fenetre
-window.title("Play Piano") #titre fenetre
+# Initialize tkinter window
+window = tkinter.Tk()
+window.title("Play Piano")
 window.geometry('1080x720')
-window.minsize(480,360)
+window.minsize(480, 360)
 window.config(background="#cccccc")
 
-#Frames
-frame = tkinter.Frame(window,bg="#cccccc",bd=2,relief="sunken") #titre et sous titres
-frame_piano = tkinter.Frame(window,bg="#cccccc")
+# Frames
+frame = tkinter.Frame(window, bg="#cccccc", bd=2, relief="sunken")
+frame_piano = tkinter.Frame(window, bg="#cccccc")
 
-#Titre
-label_title = tkinter.Label(frame,text="Jouez au piano avec le clavier", font=('Courrier',35),bg="#cccccc")
+# Title
+label_title = tkinter.Label(frame, text="Jouez au piano avec le clavier", font=('Courrier', 35), bg="#cccccc")
 label_title.pack()
 
-#Sous Titre
-label_subtitle = tkinter.Label(frame,text="3 octaves disponibles !", font=('Courrier',15),bg="#cccccc")
+# Subtitle
+label_subtitle = tkinter.Label(frame, text="3 octaves disponibles !", font=('Courrier', 15), bg="#cccccc")
 label_subtitle.pack()
 
-#bouton start
-button_start = tkinter.Button(frame,text="START",fg = "black", bg= "white",command=piano)
-button_start.pack()
-
-#affichage
+# Display frames
 frame.pack(side="top")
 frame_piano.pack(expand=True)
 
-def start(window):
-    window.mainloop()
+# Bind the key press event to the `key_press` function
+window.bind("<KeyPress>", key_press)
 
-threadpiano = Thread(target = piano,args = ())
-threadwindows = Thread(target = start, args = (windows))
-                       
-threadpiano.start()
-threadwindows.start()
+# Start tkinter's main loop (this must run in the main thread)
+window.mainloop()
